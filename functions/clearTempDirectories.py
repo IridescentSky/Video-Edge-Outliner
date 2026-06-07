5e4acc751721ae13bd5305dbe423ec4ecc185852
Sleepyleaf58 <frankhuang132@gmail.com>
2026-06-06 22:43:06 -0400

    Polishing and undoing last commit since EXE in repo is bad practice

--- functions/clearTempDirectories.py
+++ functions/clearTempDirectories.py
@@ -2,9 +2,4 @@ import shutil
 import os
 
 def clearTempDirectories(frames_path: str):
-    # Clearing temp frame directories
-    original_dir = os.path.join(frames_path, "original")
-    canny_dir = os.path.join(frames_path, "canny")
-
-    shutil.rmtree(original_dir)
-    shutil.rmtree(canny_dir)
+    shutil.rmtree(frames_path)
